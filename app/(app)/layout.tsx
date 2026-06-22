5f7fc0c6119484a68f51fdb3d5691e49f07a56e7
Nasrul Fatah <nazfat0@gmail.com>
2026-06-22 21:05:59 +0700

    Feature: add ProductProvider and other context providers to app layout

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,9 +1,21 @@
 import { StoreProvider } from '@/src/context/StoreContext';
+import { ProductProvider } from '@/src/context/ProductContext';
+import { OrderProvider } from '@/src/context/OrderContext';
+import { CartProvider } from '@/src/context/CartContext';
+import { WishlistProvider } from '@/src/context/WishlistContext';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <StoreProvider>
-      {children}
+      <ProductProvider>
+        <OrderProvider>
+          <CartProvider>
+            <WishlistProvider>
+              {children}
+            </WishlistProvider>
+          </CartProvider>
+        </OrderProvider>
+      </ProductProvider>
     </StoreProvider>
   );
 }
